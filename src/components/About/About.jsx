bca96e2bf22240d92a6db629d6c89a01a563ac8f
PriyanshiAmin197 <priyanshiamin197@gmail.com>
2025-02-12 16:54:59 -0500

    correction

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -9,7 +9,7 @@ export const About = () => {
       <h2 className={styles.title}>About</h2>
       <div className={styles.content}>
         <img
-          src="./react-portfolio/assets/about/aboutImage.png"
+          src="./assets/about/aboutImage.png"
           alt="Me sitting with a laptop"
           className={styles.aboutImage}
         />
