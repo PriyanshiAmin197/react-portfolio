4e3f2479a754d25c27f076d4095fddc9b522c3f4
PriyanshiAmin197 <priyanshiamin197@gmail.com>
2025-02-12 16:47:10 -0500

    correction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -12,7 +12,7 @@ export const Navbar = () => {
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
 
-            <img className={styles.menuBtn} src={menuOpen ?  "./assets/nav/closeIcon.png" : "./assets/nav/menuIcon.png"} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
+            <img className={styles.menuBtn} src={menuOpen ?  "./react-portfolio/assets/nav/closeIcon.png" : "./react-portfolio/assets/nav/menuIcon.png"} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
 
                 <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
                     <li>
