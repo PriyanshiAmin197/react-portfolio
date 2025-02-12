b8376fdb3454d8ab0e04fdde4df52a9136a34140
PriyanshiAmin197 <priyanshiamin197@gmail.com>
2025-02-12 16:26:47 -0500

    correction

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -12,18 +12,18 @@ export const Contact = () => {
       </div>
       <ul className={styles.links}>
         <li className={styles.link}>
-          <img src={"./react-portfolio/assets/contact/emailIcon.png"} alt="Email icon" />
+          <img src={"./assets/contact/emailIcon.png"} alt="Email icon" />
           <a href="mailto:patelpriyanshi197@gmail.com">patelpriyanshi197@gmail.com</a>
         </li>
         <li className={styles.link}>
           <img
-            src={"./react-portfolio/assets/contact/linkedinIcon.png"}
+            src={"./assets/contact/linkedinIcon.png"}
             alt="LinkedIn icon"
           />
           <a href="https://www.linkedin.com/in/priyanshi-amin-006515193/">linkedin.com/priyanshi-amin</a>
         </li>
         <li className={styles.link}>
-          <img src={"./react-portfolio/assets/contact/githubIcon.png"} alt="Github icon" />
+          <img src={"./assets/contact/githubIcon.png"} alt="Github icon" />
           <a href="https://github.com/PriyanshiAmin197">github.com/PriyanshiAmin197</a>
         </li>
       </ul>
