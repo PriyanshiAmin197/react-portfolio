da915d1454231a194f889e83c04d003c27f5c247
PriyanshiAmin197 <priyanshiamin197@gmail.com>
2025-02-21 15:24:01 -0500

    correction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 
 import styles from "./Navbar.module.css";
-import {getImageUrl} from "../../utils";
+import closeIcon from '../../../assets/nav/closeIcon.png';
+import menuIcon from '../../../assets/nav/menuIcon.png';
 
 
 export const Navbar = () => {
@@ -12,7 +13,7 @@ export const Navbar = () => {
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
 
-            <img className={styles.menuBtn} src={menuOpen ?  "./react-portfolio/assets/nav/closeIcon.png" : "./react-portfolio/assets/nav/menuIcon.png"} alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
+            <img className={styles.menuBtn} src={menuOpen ? closeIcon : menuIcon } alt="menu-button" onClick={() => setMenuOpen(!menuOpen)} />
 
                 <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`} onClick={() => setMenuOpen(false)}>
                     <li>
