ac1350afa32f40434d0c6a0f0ca7e6ec2d113332
PriyanshiAmin197 <priyanshiamin197@gmail.com>
2025-05-22 15:25:31 -0400

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "react-portfolio",
-  css: {
-    modules: {
-      localsConvention: "camelCase"
-    }
-  }
+  base: process.env.VITE_BASE_PATH || "/react-vite-deploy",
 })
